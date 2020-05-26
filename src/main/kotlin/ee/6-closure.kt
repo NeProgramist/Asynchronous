e15b7ca8c647dd20d856ba859fc82ecda464a757
ALEGATOR1209 <alekos1209@gmail.com>
2020-05-26 23:57:35 +0300

    EE: 6+8

---
+++ src/main/kotlin/ee/6-closure.kt
@@ -0,0 +1,27 @@
+package ee
+
+interface Emitter<K> {
+    fun on(name: String, fn: (K) -> Unit): Boolean
+    fun emit(name: String, data: K)
+}
+
+fun <K> emitter(): Emitter<K> {
+    val events = mutableMapOf<String, MutableList<(K) -> Unit>>()
+
+    return object: Emitter<K> {
+        override fun on(name: String, fn: (K) -> Unit) = events
+        .getOrPut(name) { mutableListOf() }
+        .add(fn)
+
+        override fun emit(name: String, data: K) = events
+            .getOrDefault(name, null)
+            ?.forEach { it(data) } ?: Unit
+    }
+}
+
+// Usage
+fun main() {
+    val ee = emitter<String>()
+    ee.on("event1") { println("RECEIVED: $it") }
+    ee.emit("event1", "data")
+}
