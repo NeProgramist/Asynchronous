ec923a089d6691fe4448b1eaec4e86c4277d555a
NeProgramist <zasko180@gmail.com>
2020-05-26 19:37:54 +0300

    add sleep implementation

--- src/main/kotlin/timers/1-sleep.kt
+++ src/main/kotlin/timers/1-sleep.kt
@@ -1,5 +1,14 @@
 package timers
 
-fun main() {
+import java.util.*
 
+fun main() {
+    println("start")
+    sleep(3000)
+    println("end")
+}
+
+fun sleep(msec: Int) {
+    val end = Date().time + msec
+    while (Date().time < end) {}
 }
