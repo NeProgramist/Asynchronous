f67a50c1241946bcb29f5007fa83887dc65200be
NeProgramist <zasko180@gmail.com>
2020-05-26 20:26:11 +0300

    add imp of sleep-then

--- src/main/kotlin/timers/3-sleep-then.kt
+++ src/main/kotlin/timers/3-sleep-then.kt
@@ -1,9 +1,20 @@
 package timers
 
-fun main() {
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
 
+suspend fun main() {
+    println("start ${Date()}")
+    val job = sleep(3000)
+    job.invokeOnCompletion {
+        println("end ${Date()}")
+    }
+
+    delay(10000)
 }
 
-private fun sleep(msec: Int) {
-
+private fun sleep(msec: Long) = GlobalScope.launch {
+    delay(msec)
 }
