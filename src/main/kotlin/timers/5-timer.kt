52a41c885d942d1cef6894dd245fdb0bef0b1f3d
NeProgramist <zasko180@gmail.com>
2020-05-26 20:55:27 +0300

    add timer imp

---
+++ src/main/kotlin/timers/5-timer.kt
@@ -0,0 +1,49 @@
+package timers
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class Timer(private val interval: Long) {
+    private var enabled = false
+    private val listeners = mutableListOf<() -> Unit>()
+    private lateinit var job: Job
+
+    fun on(name:String, func: () -> Unit) {
+        if (name == "timer") listeners.add(func)
+    }
+
+    fun start() {
+        if (!enabled) {
+            enabled = true
+            job = GlobalScope.launch {
+                delay(interval)
+                for (func in listeners) func()
+            }
+        }
+        println("timer start")
+    }
+
+    fun stop() {
+        if (enabled) {
+            job.cancel()
+            enabled = false
+            println("timer stop")
+        }
+    }
+}
+
+suspend fun main() {
+    val timer = Timer(3000)
+
+    timer.on("timer") { println("i'm here") }
+    timer.on("timer") { println("i'm here x2") }
+    timer.on("not a timer") { println("i'm not here") }
+
+    timer.start()
+    delay(2000)
+    timer.stop()
+    timer.start()
+    delay(10000)
+}
