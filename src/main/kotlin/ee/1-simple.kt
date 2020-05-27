6aeef4e0272a00ec074dcc60a47ee184882961d5
NeProgramist <zasko180@gmail.com>
2020-05-27 15:02:26 +0300

    change name to SimpleEmitter

--- src/main/kotlin/ee/1-simple.kt
+++ src/main/kotlin/ee/1-simple.kt
@@ -1,14 +1,14 @@
 package ee
 
 fun main() {
-    val ee = EventEmitter<String>()
+    val ee = SimpleEmitter<String>()
     ee.on("first") { data -> println("first data - $data") }
     ee.on("first") { data -> println("second data - $data") }
 
     ee.emit("first", "i'm here")
 }
 
-class EventEmitter<T> {
+class SimpleEmitter<T> {
     private val events = mutableMapOf<String, MutableList<(T) -> Unit>>()
 
     fun on(name: String, func: (T) -> Unit) = events.getOrPut(name, { mutableListOf() }).add(func)
