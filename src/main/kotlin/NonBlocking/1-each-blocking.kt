7e8c8361d6949c59575dcab66974fcaa6f53f137
NeProgramist <zasko180@gmail.com>
2020-05-29 17:11:35 +0300

    minor fix

--- src/main/kotlin/NonBlocking/1-each-blocking.kt
+++ src/main/kotlin/NonBlocking/1-each-blocking.kt
@@ -1,2 +1,17 @@
 package NonBlocking
 
+import kotlinx.coroutines.delay
+import kotlin.system.measureTimeMillis
+
+
+val numbers = List(1000) {it}
+
+suspend fun main() {
+    val time = measureTimeMillis {
+        numbers.forEach { println(it) }
+        delay(1)
+        println("i'm here")
+    }
+
+    println("Blocking $time")
+}
