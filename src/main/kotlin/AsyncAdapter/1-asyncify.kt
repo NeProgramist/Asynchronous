4334d95c77c5d7e4a6e280bd4411515422e3d444
Zhenya <zasko180@gmail.com>
2020-05-31 15:24:15 +0300

    feat: add impl of async-adapters [./AsyncAdapters/*.kt] (#16)
    * add impl of asyncify
    
    * built: add linter
    
    * refactor: add tests [1-async]
    
    * feat: add impl of deferred to cb in kotlin [AsyncAdapter/5-deferred-to-cb]

--- src/main/kotlin/AsyncAdapter/1-asyncify.kt
+++ src/main/kotlin/AsyncAdapter/1-asyncify.kt
@@ -1,3 +1,34 @@
 package AsyncAdapter
 
-typealias Errback<T> = (Error?, T) -> Unit
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+typealias Errback<T> = (Error?, T?) -> Unit
+
+fun <T, K> asyncify(fn: (T) -> K) = { arg:T, callback: Errback<K> ->
+    GlobalScope.launch {
+        val res = fn(arg)
+        if (res is Error) callback(res, null)
+        else callback(null, res)
+    }
+}
+
+private fun main() = runBlocking{
+    val twice = { x: Float -> x*2 }
+    val twiceAsync = asyncify(twice)
+
+    val half = { x: Float -> x/2 }
+    val halfAsync = asyncify(half)
+
+    val result = half(twice(100f))
+    println("sync res: $result")
+
+    twiceAsync(100f) { err1, value ->
+        if (err1 != null) println("the error occurred(twice): $err1")
+        halfAsync(value!!) { err2, result ->
+            if (err2 != null) println("the error occurred(half): $err2")
+            else println("async res: $result")
+        }
+    }.join()
+}
