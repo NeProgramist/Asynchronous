c4660f8eb4d617763305833a49ed213b12669cfb
NeProgramist <zasko180@gmail.com>
2020-06-01 21:42:06 +0300

    fix(core): change Future interface [1-lazy.kt]

--- src/main/kotlin/Future/1-lazy.kt
+++ src/main/kotlin/Future/1-lazy.kt
@@ -1,7 +1,23 @@
 package Future
 
-interface Furure {
-    fun map()
-    fun fork()
-    fun chain()
+interface Future<T> {
+    fun map(fn: (T) -> T): Future<T>
+    fun fork(fn: (T) -> Unit): Future<T>
+}
+
+private fun <T> future(value: T): Future {
+    val mapper: (T) -> T
+    return object: Future {
+        override fun map() {
+        }
+
+        override fun fork() {
+        }
+
+        override fun chain() {}
+    }
+}
+
+private fun main() {
+
 }
