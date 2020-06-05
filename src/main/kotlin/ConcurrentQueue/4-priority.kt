909e608a5dba6ca81443ce77485a2c24b45f0723
NeProgramist <zasko180@gmail.com>
2020-06-06 02:18:56 +0300

    feat(package): add impl of priority async queue [4-priority.kt]

---
+++ src/main/kotlin/ConcurrentQueue/4-priority.kt
@@ -0,0 +1,58 @@
+package ConcurrentQueue
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.time.LocalDateTime
+
+open class PriorityTask(
+    name: String,
+    interval: Long,
+    startTime: LocalDateTime = LocalDateTime.MIN,
+    waitTimeout: Long = Long.MAX_VALUE,
+    val priority: Int = 0
+): TimedTask(name, interval, startTime, waitTimeout)  {
+    override fun toString(): String = name
+}
+
+class PriorityQueue<T: PriorityTask>(
+    concurrency: Int,
+    scope: CoroutineScope = GlobalScope
+): TimeoutQueue<T>(concurrency, scope) {
+    protected var priorityMode = false
+
+    override fun add(task: T) {
+        super.add(task)
+        if (priorityMode) {
+            waiting.sortByDescending(PriorityTask::priority)
+        }
+    }
+
+    fun priority(flag: Boolean = true): PriorityQueue<T> {
+        priorityMode = flag
+        if (flag) waiting.sortByDescending(PriorityTask::priority)
+        return this
+    }
+}
+
+
+private fun main() = runBlocking {
+    suspend fun job(task: PriorityTask, next: Errback<PriorityTask>) {
+        delay(task.interval)
+        next(null, task)
+    }
+
+    val queue = PriorityQueue<PriorityTask>(3).apply {
+        process(::job)
+        priority()
+        done { error, task ->
+            if (error != null) println("Done with error: $error")
+            else println("Done: ${task.name}, count: $count, waiting: ${waiting.size}")
+        }
+        drain { println("Queue drain") }
+    }
+
+    for (i in 0 until 10) queue.add(PriorityTask("Task$i", 1000L, priority = i))
+    delay(15000)
+}
